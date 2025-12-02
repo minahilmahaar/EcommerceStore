9990cff17bb11121ee6f5e53cc2b2f73277b57b1
minahilmahaar <minahilmahaar@gmail.com>
2025-12-02 07:36:31 -0800

    new project

--- src/app/unsubscribe/unsubscribe.ts
+++ src/app/unsubscribe/unsubscribe.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { interval, Subscription } from 'rxjs';
 @Component({
   selector: 'app-unsubscribe',
   standalone:true,
@@ -8,6 +9,37 @@ import { CommonModule } from '@angular/common';
   styleUrl: './unsubscribe.css',
 })
 export class Unsubscribe {
-numbers: number[] = [1, 2, 3, 4, 5];
+
+counter =  interval(1000);
+data1: number[] = [];
+data2: number[] = [];
+data3: number[] = [];
+subscribe1: Subscription;
+subscribe2: Subscription;
+subscribe3: Subscription;
+OnSubscribe1(){
+this.subscribe1 = this.counter.subscribe((val) => {
+  this.data1.push(val)
+})
+}
+OnSubscribe2(){
+this.subscribe2 = this.counter.subscribe((val) => {
+  this.data2.push(val)
+})
+}
+OnSubscribe3(){
+this.subscribe3 = this.counter.subscribe((val) => {
+  this.data3.push(val)
+})
+}
+OnUnSubscribe1(){
+this.subscribe1?.unsubscribe();
+}
+OnUnSubscribe2(){
+this.subscribe2?.unsubscribe();
+}
+OnUnSubscribe3(){
+this.subscribe3?.unsubscribe();
+}
 
 }
